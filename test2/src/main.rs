22497e2aaecc7d5b5586cea483e98808f2c79ba1
Antidote1911 <antidote1911@gmail.com>
2025-04-03 19:56:03 +0200

    up

--- test2/src/main.rs
+++ test2/src/main.rs
@@ -1,3 +1,4 @@
+use anyhow::anyhow;
 use aead::{Aead, Payload};
 
 use chacha20poly1305::{
@@ -12,7 +13,9 @@ use std::fs::File;
 use std::io::{self, Read, Write};
 
 use std::time::{SystemTime, UNIX_EPOCH};
+use argon2::Argon2;
 use rand::Rng;
+
 // The nonce size for *BE32 is 5-bytes smaller (32-bit counter + last block flag byte) than the 24-byte XChaCha20Poly1305 nonce so 24 - 5 = 19 bytes.
 // You need to use GenericArray<u8, U19>.
 
@@ -22,18 +25,21 @@ const SALT_SIZE: usize = 16;
 const CHUNK_SIZE: usize = 1024 * 1024; // 1 MB
 
 
-fn encrypt_file<P: AsRef<Path>>(input_path: P, output_path: P,key:&[u8]) -> Result<(), Box<dyn std::error::Error>> {
+fn encrypt_file<P: AsRef<Path>>(input_path: P, output_path: P, key: &[u8]) -> Result<(), anyhow::Error>  {
 
     //let key   : Key               = XChaCha20Poly1305::generate_key(&mut OsRng);
     //let nonce : XNonce            = XChaCha20Poly1305::generate_nonce(&mut OsRng);
 
-    let salt: [u8; SALT_SIZE] = rand::rng().random();
-    let nonce: [u8; NONCE_SIZE] = rand::rng().random();
+    let mut salt: [u8; SALT_SIZE] = rand::rng().random();
+    let mut nonce: [u8; NONCE_SIZE] = rand::rng().random();
+
+    let mut derivedKey = [0u8; 32]; // Can be any desired size
+    Argon2::default().hash_password_into(key, &salt, &mut derivedKey).unwrap();
 
     //let nonce= XNonce::from_slice(NONCE);
 
     // create the stream encryptor
-    let cipher = XChaCha20Poly1305::new(key.into());
+    let cipher = XChaCha20Poly1305::new((&derivedKey).into());
     let mut stream_encryptor = stream::EncryptorBE32::from_aead(cipher, &nonce.into());
 
     let mut input_file = File::open(input_path)?;
@@ -44,17 +50,22 @@ fn encrypt_file<P: AsRef<Path>>(input_path: P, output_path: P,key:&[u8]) -> Resu
     output_file.write_all(&nonce)?;
 
     let mut buffer = [0u8; CHUNK_SIZE];
-    while let Ok(read_bytes) = input_file.read(&mut buffer){
-        if read_bytes == 0 {
+
+    loop {
+        let read_count = input_file.read(&mut buffer)?;
+
+        if read_count == CHUNK_SIZE {
+            let ciphertext = stream_encryptor
+                .encrypt_next(buffer.as_slice())
+                .map_err(|err| anyhow!("Encrypting file: {}", err))?;
+            output_file.write(&ciphertext)?;
+        } else {
+            let ciphertext = stream_encryptor
+                .encrypt_last(&buffer[..read_count])
+                .map_err(|err| anyhow!("Encrypting file: {}", err))?;
+            output_file.write(&ciphertext)?;
             break;
         }
-
-        let encrypted_chunk = stream_encryptor.encrypt_next(Payload {
-            msg: &buffer[..read_bytes],
-            aad: b"",
-        });
-
-        output_file.write_all(&encrypted_chunk.unwrap())?;
     }
     Ok(())
 }
@@ -70,37 +81,45 @@ fn decrypt_file<P: AsRef<Path>>(input_path: P, output_path: P,key:&[u8]) -> Resu
     input_file.read_exact(&mut salt)?;
     input_file.read_exact(&mut nonce)?;
 
+    let mut derivedKey = [0u8; 32]; // Can be any desired size
+    Argon2::default().hash_password_into(key, &salt, &mut derivedKey).unwrap();
+
     // create the stream decryptor
-    let cipher = XChaCha20Poly1305::new(key.into());
+    let cipher = XChaCha20Poly1305::new((&derivedKey).into());
     let mut stream_decryptor = stream::DecryptorBE32::from_aead(cipher, &nonce.into());
 
     let mut buffer = [0u8; CHUNK_SIZE + 16]; // CHUNK_SIZE bytes data + 16 bytes tag
-    while let Ok(read_bytes) = input_file.read(&mut buffer) {
-        if read_bytes == 0 {
+    loop {
+        let read_count = input_file.read(&mut buffer)?;
+
+        if read_count == buffer.len() {
+            let plaintext = stream_decryptor
+                .decrypt_next(buffer.as_slice())
+                .map_err(|err| anyhow!("Decrypting large file: {}", err))?;
+            output_file.write(&plaintext)?;
+        } else if read_count == 0 {
+            break;
+        } else {
+            let plaintext = stream_decryptor
+                .decrypt_last(&buffer[..read_count])
+                .map_err(|err| anyhow!("Decrypting large file: {}", err))?;
+            output_file.write(&plaintext)?;
             break;
         }
-
-        let decrypted_chunk = stream_decryptor.decrypt_next(Payload {
-            msg: &buffer[..read_bytes],
-            aad: b"",
-        });
-        output_file.write_all(&decrypted_chunk.unwrap())?;
     }
     Ok(())
 }
 
-
 fn main() -> io::Result<()> {
-    let keyslice = b"an example very very secret key."; // 32 bytes
-    let key = aead::Key::<XChaCha20Poly1305>::from_slice(keyslice);
+    let password = b"coucou!!";
 
     let input_path = "5.mp4";
     let encrypted_path = "5.bin";
     let decrypted_path = "decrypted.mp4";
 
     let start = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
-    encrypt_file(input_path, encrypted_path,key).expect("Encryption failed");
-    decrypt_file(encrypted_path, decrypted_path,key).expect("Decryption failed");
+    encrypt_file(input_path, encrypted_path,password).expect("Encryption failed");
+    decrypt_file(encrypted_path, decrypted_path,password).expect("Decryption failed");
     let end = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
 
     let test=end-start;
