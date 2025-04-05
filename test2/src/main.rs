090ce357ad85144e5e1dabc5eb4b37b99b70e96e
Antidote1911 <antidote1911@gmail.com>
2025-04-05 08:52:05 +0200

    up

--- test2/src/main.rs
+++ test2/src/main.rs
@@ -13,8 +13,11 @@ use std::fs::File;
 use std::io::{self, BufReader, BufWriter, Read, Write};
 
 use std::time::{SystemTime, UNIX_EPOCH};
-use argon2::Argon2;
+use argon2::{Algorithm, Argon2, Params, ParamsBuilder, Version};
 use rand::Rng;
+
+use indicatif::ProgressBar;
+use indicatif::ProgressStyle;
 
 // The nonce size for *BE32 is 5-bytes smaller (32-bit counter + last block flag byte) than the 24-byte XChaCha20Poly1305 nonce so 24 - 5 = 19 bytes.
 // You need to use GenericArray<u8, U19>.
@@ -27,7 +30,7 @@ const TAG_LEN: usize = 16;
 const ENCRYPTION_CHUNK_SIZE: usize = 1024 * 512; // 1 MB
 const DECRYPTION_CHUNK_SIZE: usize = ENCRYPTION_CHUNK_SIZE + TAG_LEN;
 
-fn encrypt_file<P: AsRef<Path>>(input_path: P, output_path: P, key: &[u8]) -> Result<(), anyhow::Error>  {
+fn encrypt_file<P: AsRef<Path>>(input_path: P, output_path: P, password: &str) -> Result<(), anyhow::Error>  {
 
     //let key   : Key               = XChaCha20Poly1305::generate_key(&mut OsRng);
     //let nonce : XNonce            = XChaCha20Poly1305::generate_nonce(&mut OsRng);
@@ -40,16 +43,15 @@ fn encrypt_file<P: AsRef<Path>>(input_path: P, output_path: P, key: &[u8]) -> Re
     println!("Salt  {}: Hex:  {}", salt.len(),hexsalt);
     println!("Nonce {}: Hex:  {}", nonce.len(),hexnonce);
 
-    let mut derived_key = [0u8; 32]; // Can be any desired size
-    Argon2::default().hash_password_into(key, &salt, &mut derived_key).unwrap();
+    let key= argon2_hash(password.into(),&salt)?;
 
-    let hexkey= hex::encode_upper(derived_key);
-    println!("Key   {}: Hex:  {}", derived_key.len(),hexkey);
+    let hexkey= hex::encode_upper(key);
+    println!("Key   {}: Hex:  {}", key.len(),hexkey);
 
     //let nonce= XNonce::from_slice(NONCE);
 
     // create the stream encryptor
-    let cipher = XChaCha20Poly1305::new((&derived_key).into());
+    let cipher = XChaCha20Poly1305::new(&key);
     let mut stream_encryptor = stream::EncryptorBE32::from_aead(cipher, &nonce.into());
 
     let mut input_file = File::open(input_path)?;
@@ -61,6 +63,13 @@ fn encrypt_file<P: AsRef<Path>>(input_path: P, output_path: P, key: &[u8]) -> Re
 
     let mut buffer = [0u8; ENCRYPTION_CHUNK_SIZE];
 
+    let metadata = input_file.metadata()?;
+    let total_size = metadata.len();
+    let progress_bar = ProgressBar::new(total_size);
+    progress_bar.set_style(ProgressStyle::default_bar()
+        .template("{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({bytes_per_sec}, {eta})")
+        .progress_chars("#>-"));
+
     loop {
         let read_count = input_file.read(&mut buffer)?;
 
@@ -69,17 +78,20 @@ fn encrypt_file<P: AsRef<Path>>(input_path: P, output_path: P, key: &[u8]) -> Re
                 .encrypt_next(buffer.as_slice())
                 .map_err(|err| anyhow!("Encrypting file: {}", err))?;
             output_file.write_all(&ciphertext)?;
+            progress_bar.inc(read_count as u64);
         } else {
             let ciphertext = stream_encryptor
                 .encrypt_last(&buffer[..read_count])
                 .map_err(|err| anyhow!("Encrypting file: {}", err))?;
             output_file.write_all(&ciphertext)?;
+            progress_bar.inc(read_count as u64);
             break;
         }
     }
+    progress_bar.finish_with_message("Encryption complete");
     Ok(())
 }
-fn decrypt_file<P: AsRef<Path>>(input_path: P, output_path: P,key:&[u8]) -> Result<(), anyhow::Error> {
+fn decrypt_file<P: AsRef<Path>>(input_path: P, output_path: P,password:&str) -> Result<(), anyhow::Error> {
 
     let mut input_file = File::open(input_path)?;
     let mut output_file = File::create(output_path)?;
@@ -95,17 +107,24 @@ fn decrypt_file<P: AsRef<Path>>(input_path: P, output_path: P,key:&[u8]) -> Resu
     println!("Salt  {}: Hex:  {}", salt.len(),hexsalt);
     println!("Nonce {}: Hex:  {}", nonce.len(),hexnonce);
 
-    let mut derived_key = [0u8; 32]; // Can be any desired size
-    Argon2::default().hash_password_into(key, &salt, &mut derived_key).unwrap();
+    let derived_key = argon2_hash(password,&salt)?;
 
     let hexkey= hex::encode_upper(derived_key);
     println!("Key   {}: Hex:  {}", derived_key.len(),hexkey);
 
     // create the stream decryptor
-    let cipher = XChaCha20Poly1305::new((&derived_key).into());
+    let cipher = XChaCha20Poly1305::new(&derived_key);
     let mut stream_decryptor = stream::DecryptorBE32::from_aead(cipher, &nonce.into());
 
     let mut buffer = [0u8; DECRYPTION_CHUNK_SIZE]; // CHUNK_SIZE bytes data + 16 bytes tag
+
+    let metadata = input_file.metadata()?;
+    let total_size = metadata.len();
+    let progress_bar = ProgressBar::new(total_size);
+    progress_bar.set_style(ProgressStyle::default_bar()
+        .template("{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({bytes_per_sec}, {eta})")
+        .progress_chars("#>-"));
+
     loop {
         let read_count = input_file.read(&mut buffer)?;
 
@@ -114,6 +133,7 @@ fn decrypt_file<P: AsRef<Path>>(input_path: P, output_path: P,key:&[u8]) -> Resu
                 .decrypt_next(buffer.as_slice())
                 .map_err(|err| anyhow!("Decrypting large file: {}", err))?;
             output_file.write(&plaintext)?;
+            progress_bar.inc(read_count as u64);
         } else if read_count == 0 {
             break;
         } else {
@@ -121,14 +141,34 @@ fn decrypt_file<P: AsRef<Path>>(input_path: P, output_path: P,key:&[u8]) -> Resu
                 .decrypt_last(&buffer[..read_count])
                 .map_err(|err| anyhow!("Decrypting large file: {}", err))?;
             output_file.write(&plaintext)?;
+            progress_bar.inc(read_count as u64);
             break;
         }
     }
+    progress_bar.finish_with_message("Decryption complete");
     Ok(())
 }
 
+pub fn argon2_hash(
+    password: &str,
+    salt: &[u8; SALT_SIZE]
+) -> Result<Key, anyhow::Error> {
+
+    let mut builder = ParamsBuilder::new();
+    builder.m_cost(1024 * 100);
+    builder.t_cost(8);
+    builder.p_cost(4);
+    builder.output_len(32);
+
+    let argon2 = Argon2::new(Algorithm::Argon2id, Version::V0x13, builder.build().unwrap());
+    let mut keyarray = [0u8; 32];
+    argon2.hash_password_into(password.as_ref(), salt, &mut keyarray).map_err(|err| anyhow!("Argon2 error: {}", err))?;
+    let key = aead::Key::<XChaCha20Poly1305>::from(keyarray);
+    Ok(key)
+}
+
 fn main() -> io::Result<()> {
-    let password = b"coucou!!";
+    let password = "coucou!!";
 
     let input_path = "5.mp4";
     let encrypted_path = "5.bin";
